e36e8256c598cc1e0bea17662281435f7ca914b2
Meda Ruthu <51402756+Ruthu417@users.noreply.github.com>
2025-03-25 16:35:23 +0530

    Add files via upload

---
+++ package/module.py
@@ -0,0 +1,13 @@
+def evenodd(n):
+    if(n%2==0):
+        print('Even')
+    else:
+        print('odd')
+n=int(input())
+
+def palindrom(n):
+    if(n[::-1]==n):
+        print("palindrom")
+    else:
+        print('not palindrom')
+n=input()
